b5336b747bd9fca3a2bf932251cf17375bac5ce2
Arturo <arturomtm@gmail.com>
2013-05-26 01:06:45 +0200

    More indentation fixed...

--- 3_net/net-koans.js
+++ 3_net/net-koans.js
@@ -41,10 +41,10 @@ var RemotePrompt = function(library){
         connection.___("Welcome to your command line playlist manager, " + remoteIP);
 		
 	if (remoteIP in sessionsDB){
-            /*
-                KOAN #5
-                should be able to close connections
-            */
+/*
+    KOAN #5
+    should be able to close connections
+*/
         connection.___("Duplicated session, closing.");
             return;
         };
@@ -87,112 +87,112 @@ var RemotePrompt = function(library){
     KOAN #7
     should receive incoming data from connections
 */
-		connection.on(___, function(data){
+        connection.on(___, function(data){
 		
-			// se deshabilita el timeout por si hubiera sido activado
-			this.setTimeout(0);
+            // se deshabilita el timeout por si hubiera sido activado
+            this.setTimeout(0);
 			
-			var command = data.toString('utf8').split("\r\n")[0];
+            var command = data.toString('utf8').split("\r\n")[0];
 
-			switch(command){
-				case "list":
-					var playlist = source.list();
-					this.write("\r\nSongs in the playlist");
-					this.write("\r\n---------------------");
-					for (var i=0; i < playlist.length; i++){
-						var song = playlist[i];
-						this.write("\r\n" + (source.currentTrack() == song? "> " : "  ") + song);
-					}
-					this.write("\r\n# ");
-					break;
-				case "play":
-					source.play();
-					break;
-				case "pause":
-					source.pause();
-					break;
-				case "next":
-					source.next();
-					break;
-				case "prev":
-					source.prev();
-					break;
-				case "exit":
-        			delete sessionsDB[this.remoteAddress];
-					this.end("Bye.");
-					break;
-				default:
-					this.write("Command " + command + " unknown\r\n# ");
-			}
-		});
+            switch(command){
+                case "list":
+                    var playlist = source.list();
+                    this.write("\r\nSongs in the playlist");
+                    this.write("\r\n---------------------");
+                    for (var i=0; i < playlist.length; i++){
+                        var song = playlist[i];
+                        this.write("\r\n" + (source.currentTrack() == song? "> " : "  ") + song);
+                    }
+                    this.write("\r\n# ");
+                    break;
+                case "play":
+                    source.play();
+                    break;
+                case "pause":
+                    source.pause();
+                    break;
+                case "next":
+                    source.next();
+                    break;
+                case "prev":
+                    source.prev();
+                    break;
+                case "exit":
+                    delete sessionsDB[this.remoteAddress];
+                    this.end("Bye.");
+                    break;
+                default:
+                    this.write("Command " + command + " unknown\r\n# ");
+            }
+        });
 
-		connection.on('close', function(){
+        connection.on('close', function(){
             source.stop();
             udpSocket.close();
             rtpprotocol = source = null;
-		});
+        });
 			
-		connection.write("\r\nNow, point your player to:\r\n\trtp://" + remoteIP + ":5002\r\n# ");
+        connection.write("\r\nNow, point your player to:\r\n\trtp://" + remoteIP + ":5002\r\n# ");
 			
-	});
+    });
 };
 
 exports.create = function(){
-	var actions = [];
+    var actions = [];
 	
-	var app;
-	var library = new nodeMp3.Mp3Library({ basedir: '../data/songs/' });
-	library.on('ready', function(){
-		app = new RemotePrompt(this);
+    var app;
+    var library = new nodeMp3.Mp3Library({ basedir: '../data/songs/' });
+    library.on('ready', function(){
+        app = new RemotePrompt(this);
 
-		for (var i = 0; i< actions.length; i++){
-			actions[i].apply(app);
-		};
-		actions = undefined;
-	});
+        for (var i = 0; i< actions.length; i++){
+            actions[i].apply(app);
+        };
+        actions = undefined;
+    });
 
-	// TO COMPLETELY IGNORE: 
-	// some kind of ugly, lame & messy code mixing promise pattern with proxy
+    // TO COMPLETELY IGNORE: 
+    // some kind of ugly, lame & messy code mixing promise pattern with proxy
     // pattern.
     //
-	// This offers an object with same listen method as RemotePrompt class
+    // This offers an object with same listen method as RemotePrompt class
     // invocable when app is not ready yet. It offers too an onListening method
     // that will install a callback on 'listening' event on server property of
     // RemotePrompt; this method should not be in RemotePrompt class itself but
     // it's useful for testing purposes. It's also chainable.
-	return new function(){
-		var _defer = function(callback){
-			if (actions){
-				actions.push(callback);
-			} else {
-				callback.apply(app)
-			}
-		};
+    return new function(){
+        var _defer = function(callback){
+            if (actions){
+                actions.push(callback);
+            } else {
+                callback.apply(app)
+            }
+        };
 		
-		var self = this;
-		this.listen = function(port){
-			_defer(function(){
-				this.listen(port);
-			})
-		};
+        var self = this;
+        this.listen = function(port){
+            _defer(function(){
+                this.listen(port);
+            })
+        };
 		
-		this.close = function(){
-			_defer(function(){
-				this.close();
-			})
-		};
+        this.close = function(){
+            _defer(function(){
+                this.close();
+            })
+        };
 		
-		this.onListening = function(callback){
-			_defer(function(){
-				this.server.on('listening', callback);
-			});
-			return self;
-		}
+        this.onListening = function(callback){
+            _defer(function(){
+                this.server.on('listening', callback);
+            });
+            return self;
+        }
 
         this.getServer = function(callback){
             _defer(function(){
                 callback(this.server);
             });
         }
-	}
+    }
 };
